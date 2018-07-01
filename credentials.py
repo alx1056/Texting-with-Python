9a14337c0f757aa33099959ba4b582603232551b
Alex Fields <alx1056@hotmail.com>
2018-07-01 15:40:49 -0400

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,5 @@
+#for security purposes I cannot show this
+account_sid = '################################'
+auth_token = '###################################'
+my_cell = '###-###-####'
+my_twilio = '+18284710639'
